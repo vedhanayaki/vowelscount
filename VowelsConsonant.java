9292a4fccdc9802f6a8da724936a7e2f4de0e6d4
vedhanayaki <vedhanayaki1996@gmail.com>
2017-07-17 01:59:26 -0700

    Add files via upload

---
+++ VowelsConsonant.java
@@ -0,0 +1,14 @@
+package player;
+import java.util.Scanner;
+public class VowelsConsonant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Enter the String:");
+Scanner s=new Scanner(System.in);
+String a=s.next();
+
+
+	}
+
+}
